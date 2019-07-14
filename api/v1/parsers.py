62dc79d9d61405f279b1a15416a6917224e3f67a
Will Button <will@willbutton.co>
2019-07-14 12:49:12 -0700

    Add initial support for uploading files + metadata

--- api/v1/parsers.py
+++ api/v1/parsers.py
@@ -1,7 +1,16 @@
+from werkzeug.datastructures import FileStorage
 from flask_restplus import reqparse
 
-pagination_arguments = reqparse.RequestParser()
-pagination_arguments.add_argument('page', type=int, required=False, default=1, help='Page number')
-pagination_arguments.add_argument('bool', type=bool, required=False, default=1, help='Page number')
-pagination_arguments.add_argument('per_page', type=int, required=False, choices=[2, 10, 20, 30, 40, 50],
+endpoint_arguments = reqparse.RequestParser()
+endpoint_arguments.add_argument('page', type=int, required=False, default=1, help='Page number')
+endpoint_arguments.add_argument('bool', type=bool, required=False, default=1, help='Page number')
+endpoint_arguments.add_argument('per_page', type=int, required=False, choices=[2, 10, 20, 30, 40, 50],
                                   default=10, help='Results per page {error_msg}')
+endpoint_arguments.add_argument('file', type=FileStorage, location='files',
+                                  required=False, help='Listing asset')
+
+listing_arguments = reqparse.RequestParser(bundle_errors=True)
+listing_arguments.add_argument('file_upload', type=FileStorage, required=True, location='files', help='Listing file asset')
+listing_arguments.add_argument('file_type', type=str, required=True, location='form', help='File type: image, video, audio, etc')
+listing_arguments.add_argument('md5_sum', type=str, required=True, location='form', help='MD5 of file asset')
+listing_arguments.add_argument('tags', type=str, required=False, location='form', action='split', help='Comma delimited list of tags for discovery')
--- settings.py
+++ settings.py
@@ -1,3 +1,45 @@
+import os
+import json
+import boto3
+from botocore.exceptions import ClientError
+
+def get_secret():
+    """
+    Retrieve application secrets from AWS Secrets Manager
+    """
+    secret_name = f"ffa/datatrust/{os.getenv('ENV', 'dev')}"
+    region_name = 'us-west-1'
+
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'DecryptionFailureException':
+            raise e
+        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidParameterException':
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidRequestException':
+            raise e
+        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
+            raise e
+        elif e.response['Error']['Code'] == 'AccessDeniedException':
+            raise e
+        else:
+            # Raise anything we didn't catch
+            raise e
+    else:
+        return json.loads(get_secret_value_response['SecretString'])
+
+secrets = get_secret()
 # Flask settings
 FLASK_DEBUG = True  # Do not use debug mode in production
 FLASK_PORT = 5000
@@ -7,3 +49,11 @@ RESTPLUS_SWAGGER_UI_DOC_EXPANSION = 'list'
 RESTPLUS_VALIDATE = True
 RESTPLUS_MASK_SWAGGER = False
 RESTPLUS_ERROR_404_HELP = False
+
+# Application settings
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_URL = secrets['DB_URL']
+TABLE_NAME = secrets['TABLE_NAME']
+S3_DESTINATION = secrets['S3_DESTINATION']
+CELERY_RESULT_BACKEND = secrets['CELERY_RESULT_BACKEND']
+CELERY_BROKER_URL = secrets['CELERY_BROKER_URL']
