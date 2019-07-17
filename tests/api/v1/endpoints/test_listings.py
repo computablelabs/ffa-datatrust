ba6d8d3b742bc04a04a5c46333b96dc0b2765fa5
Will Button <will@willbutton.co>
2019-07-16 19:52:23 -0700

    Add tests for endpoints

---
+++ tests/api/v1/endpoints/test_listings.py
@@ -0,0 +1,16 @@
+"""
+Tests for app.py
+"""
+import sys
+import pytest
+
+@pytest.fixture
+def client():
+    """
+    Flask client for testing
+    """
+    # app.app.config['TESTING'] = True
+    # app.initialize_app(app.app)
+    # client = app.app.test_client()
+    # yield client
+    pass
